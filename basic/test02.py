dddcd5a6dfe74c4be6f4227c8c451fd0b2a03dba
HiroyasuTokita <jenysbook@jenysbook.sakura.ne.jp>
2018-04-17 22:46:04 +0900

    20180417_2245
    実験。

--- basic/test02.py
+++ basic/test02.py
@@ -1,3 +1,10 @@
+# -*- coding: utf-8 -*-
+import sys
+import io
+
+sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
+sys.stderr = io.TextIOWrapper(sys.stderr.detach(), encoding = 'utf-8')
+
 print('モジュールのロード')
 
 def test():
---
+++ basic/test03.py
@@ -0,0 +1,7 @@
+
+
+print('あなたの名前を教えてください。')
+
+your_name = input('>> ')
+
+print("こんにちは！{}さん！".format(your_name))
